55f41f03abf0eca0cfde28440e2773102810d438
Nícolas R. Hackenhar <prog46@ixcsoft.com.br>
2021-10-15 14:44:55 -0300

    chore: Implementados métodos do processo de busca do conteúdo de determinado endereço passado pelo usuário.

--- cache_classes.php
+++ cache_classes.php
@@ -2,28 +2,35 @@
 
 class LinhaCache
 {
-    public static $tamanho = 8;
     private $conteudo;
     private $escrita = false;
     private $valid = false;
-    private $rotulo = null;
+    private $rotulo;
+    private $indice = 0;
 
-    public function setConteudo($conteudo)
+    public function __construct()
     {
-        $this->conteudo = $conteudo;
+        $this->conteudo[0] = null;
+        $this->conteudo[1] = null;
+        $this->conteudo[2] = null;
+        $this->conteudo[3] = null;
     }
 
-    public function getConteudo()
+    public function setConteudo($conteudo, $deslocamento)
     {
-        return $this->conteudo;
+        $this->conteudo[$deslocamento] = $conteudo;
+    }
+
+    public function getConteudo($deslocamento)
+    {
+        return $this->conteudo[$deslocamento];
     }
 
     public function setEscrita(bool $escrita)
     {
         $this->escrita = $escrita;
-        return $this;
     }
-    public function isEscrita(): bool
+    public function isEscrita()
     {
         return $this->escrita;
     }
@@ -31,10 +38,9 @@ class LinhaCache
     public function setValid(bool $valid)
     {
         $this->valid = $valid;
-        return $this;
     }
 
-    public function isValid(): bool
+    public function isValid()
     {
         return $this->valid;
     }
@@ -49,11 +55,21 @@ class LinhaCache
     {
         return $this->rotulo;
     }
+
+    public function setIndice($indice)
+    {
+        $this->indice = $indice;
+        return $this;
+    }
+
+    public function getIndice()
+    {
+        return $this->indice;
+    }
 }
 
 class LinhaMP
 {
-    public static $tamanho = 8;
     private $conteudo;
 
     public function setConteudo($conteudo)
@@ -76,21 +92,99 @@ class Bloco
         for ($i =0; $i<4; $i++)
         {
             $this->linhas[$i] = new LinhaMP();
-            $this->linhas[$i]->setConteudo(dechex(rand(0,255)));
+            $this->linhas[$i]->setConteudo(str_pad(dechex(rand(0,255)), 2, '0', STR_PAD_LEFT));
         }
+    }
+
+    public function getLinha($deslocamento)
+    {
+        return $this->linhas[$deslocamento];
     }
 }
 
 class Conjunto
 {
     private $linhas = array();
+    private $contador = 0;
+    private $cheio = false;
 
     public function __construct()
     {
-        for ($i =0; $i<2; $i++)
-        {
+        for ($i = 0; $i < 2; $i++) {
             $this->linhas[$i] = new LinhaCache();
         }
+    }
+
+    public function getLinhas()
+    {
+        return $this->linhas;
+    }
+
+    public function getLinha($rotulo, $deslocamento)
+    {
+        foreach ($this->linhas as $linhaCache)
+        {
+            if ($linhaCache->getRotulo() == $rotulo && $linhaCache->isValid())
+            {
+                return $linhaCache->getConteudo($deslocamento);
+            }
+        }
+
+        return false;
+    }
+
+    public function insertBloco(Bloco $bloco, $numeroBloco)
+    {
+        $this->linhas[$this->contador] = new LinhaCache();
+        $this->linhas[$this->contador]->setRotulo($numeroBloco);
+        for ($i = 0; $i < 4; $i++) {
+            $linhaMP = $bloco->getLinha($i);
+            $this->linhas[$this->contador]->setConteudo(
+                $linhaMP->getConteudo(),  $i
+            );
+        }
+        $this->linhas[$this->contador]->setValid(true);
+        $this->linhas[$this->contador]->setIndice($this->contador);
+        if ($this->cheio)
+        {
+            $this->linhas[$this->contador]->setIndice(1);
+        }
+        $this->contador++;
+        if ($this->contador > 1)
+        {
+            $this->contador = 0;
+        }
+    }
+
+    public function isCheio()
+    {
+        $this->cheio = true;
+        foreach ($this->linhas as $linhaCache)
+        {
+            if (empty($linhaCache->getRotulo()) && !$linhaCache->isValid())
+            {
+                $this->cheio = false;
+            }
+        }
+
+        return $this->cheio;
+    }
+
+    public function removeLinha()
+    {
+        $linhaRetorno = false;
+        foreach ($this->linhas as $indiceLinha => $linhaCache)
+        {
+            if ($linhaCache->getIndice() == 0)
+            {
+                $this->contador = $indiceLinha;
+                $linhaRetorno =  $linhaCache;
+            }elseif ($linhaCache->getIndice() == 1)
+            {
+                $linhaCache->setIndice(0);
+            }
+        }
+        return $linhaRetorno;
     }
 }
 
@@ -106,6 +200,21 @@ class MemoriaPrincipal
         }
     }
 
+    public function getBloco($numeroBloco)
+    {
+        return $this->blocos[$numeroBloco];
+    }
+
+    public function setBloco(LinhaCache $linhaCache)
+    {
+        for ($i = 0; $i < 4; $i++)
+        {
+            $linhaMP = $this->blocos[$linhaCache->getRotulo()]->getLinha($i);
+            $linhaMP->setConteudo($linhaCache->getConteudo($i));
+        }
+
+    }
+
 }
 
 class MemoriaCache
@@ -119,4 +228,52 @@ class MemoriaCache
             $this->conjuntos[$i] = new Conjunto();
         }
     }
+
+    public function getConjuntos()
+    {
+        return $this->conjuntos;
+    }
+
+    public function getConteudo(MemoriaPrincipal $MP, $endereco)
+    {
+        $endereco = bindec($endereco);
+        $numeroBloco = (int)($endereco / 4);
+        $deslocamento = $endereco % 4;
+        $quadro = $numeroBloco % 4;
+        $linhaCache = $this->conjuntos[$quadro]->getLinha($numeroBloco, $deslocamento);
+
+        if (!$linhaCache)
+        {
+            return $this->notInCache($MP, $numeroBloco, $quadro, $deslocamento);
+        }
+
+        return $linhaCache;
+    }
+
+    private function notInCache(MemoriaPrincipal $MP, $numeroBloco, $quadro, $deslocamento)
+    {
+        if ($this->conjuntos[$quadro]->isCheio())
+        {
+            return  $this->removeLinhaCache($MP, $numeroBloco, $deslocamento, $quadro);
+        }
+        return $this->insereBloco($MP, $numeroBloco, $quadro, $deslocamento);
+    }
+
+    private function removeLinhaCache(MemoriaPrincipal $MP, $numeroBloco, $deslocamento, $quadro)
+    {
+        $linhaRemovida = $this->conjuntos[$quadro]->removeLinha();
+        if ($linhaRemovida->isEscrita())
+        {
+            $MP->setBloco($linhaRemovida);
+        }
+        return $this->insereBloco($MP, $numeroBloco, $quadro, $deslocamento);
+    }
+
+    private function insereBloco(MemoriaPrincipal $MP, $numeroBloco, $quadro, $deslocamento)
+    {
+        $blocoMP = $MP->getBloco($numeroBloco);
+        $this->conjuntos[$quadro]->insertBloco($blocoMP, $numeroBloco);
+        return $this->conjuntos[$quadro]->getLinha($numeroBloco, $deslocamento);
+    }
+
 }
