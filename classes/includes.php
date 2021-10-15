0d614bfe31a215425eff061585daa81d213d7334
Nícolas R. Hackenhar <prog46@ixcsoft.com.br>
2021-10-15 16:19:49 -0300

    chore: Adicionado arquivo de includes das classes.

---
+++ classes/includes.php
@@ -0,0 +1,7 @@
+<?php
+include "classes/MemoriaCache.php";
+include "classes/MemoriaPrincipal.php";
+include "classes/Bloco.php";
+include "classes/LinhaMP.php";
+include "classes/LinhaCache.php";
+include "classes/Conjunto.php";
